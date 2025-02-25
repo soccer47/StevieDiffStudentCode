f170b222cf10a7299adf883a0ccb6540be0821ce
soccer47 <stevie.halprin@menloschool.org>
2025-02-25 12:39:09 -0800

    commit

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -26,24 +26,37 @@ public class PlagiarismChecker {
         int left;
         int up;
 
-        // boolean to represent whether character of doc1 being passed through has been
-
         // Iterate through the substring board bottom-up (tabulation approach)
         for (int i = 0; i < doc1.length(); i++) {
             for (int j = 0; j < doc2.length(); j++) {
-                // If there is a valid index above the current index, set 'up' to the index above
-                if (i > 0) {up = path[i-1][j];}
-                // Otherwise set 'up' to 0
-                else {up = 0;}
-                // If there is a valid index to the left of the current index, set 'left' to the left index
-                if (j > 0) {left = path[i][j - 1];}
-                // Otherwise set 'left' to 0
-                else {left = 0;}
-
-                path[i][j] = Math.max(left, up);
-                // Also add to the current index if the current letter matches the
+                // If the current letters of each doc match, set the current index to the upper-left diagonal index + 1
+                // Note- upper-left diagonal index represents longest substring within confines of current indexes of
+                // each of the docs, so current index should be 1 greater because matching leads to adding another
+                // letter (the current letter)
                 if (doc1.charAt(i) == doc2.charAt(j)) {
-                    path[i][j]++;
+                    // Make sure the upper left diagonal index exists
+                    if (i > 0 && j > 0) {
+                        path[i][j] = path[i-1][j-1] + 1;
+                    }
+                    // Otherwise set the current index to 1 (start of its own substring)
+                    else {
+                        path[i][j] = 1;
+                    }
+                }
+                // Otherwise take in the longest substring that has previously been found within the current indexes
+                // of each of the docs on the board
+                else {
+                    // If there is a valid index above the current index, set 'up' to the index above
+                    if (i > 0) {up = path[i-1][j];}
+                    // Otherwise set 'up' to 0
+                    else {up = 0;}
+                    // If there is a valid index to the left of the current index, set 'left' to the left index
+                    if (j > 0) {left = path[i][j - 1];}
+                    // Otherwise set 'left' to 0
+                    else {left = 0;}
+
+                    // Set the current index to the longer of the left and up indexes
+                    path[i][j] = Math.max(left, up);
                 }
             }
         }
@@ -51,62 +64,61 @@ public class PlagiarismChecker {
         // Return the length of the longest substring
         return path[doc1.length() - 1][doc2.length() - 1];
     }
-}
 
+    public static int firstDraftlongestSharedSubstring(String doc1, String doc2) {
+        // HashMaps to hold letters have been found after the last used index in each doc
+        // Keys are the letter, values are the index of the letter in the doc
+        HashMap<Character, Integer> one = new HashMap();
+        HashMap<Character, Integer> two = new HashMap();
 
-public static int firstDraftlongestSharedSubstring(String doc1, String doc2) {
-    // HashMaps to hold letters have been found after the last used index in each doc
-    // Keys are the letter, values are the index of the letter in the doc
-    HashMap<Character, Integer> one = new HashMap();
-    HashMap<Character, Integer> two = new HashMap();
+        // Int to represent index currently being used on each doc
+        int index1 = -1;
+        int index2 = -1;
 
-    // Int to represent index currently being used on each doc
-    int index1 = -1;
-    int index2 = -1;
+        // Int to represent length of current shared substring
+        int length = 0;
 
-    // Int to represent length of current shared substring
-    int length = 0;
+        // Continuously find the first letter to appear after the current index in both docs
+        while (index1 < doc1.length() - 1 && index2 < doc2.length() - 1) {
+            // Increment the current indexes by 1
+            index1++;
+            index2++;
+            // Get the next character of each doc
+            char first = doc1.charAt(index1);
+            char second = doc2.charAt(index2);
 
-    // Continuously find the first letter to appear after the current index in both docs
-    while (index1 < doc1.length() - 1 && index2 < doc2.length() - 1) {
-        // Increment the current indexes by 1
-        index1++;
-        index2++;
-        // Get the next character of each doc
-        char first = doc1.charAt(index1);
-        char second = doc2.charAt(index2);
+            // Add the letters to the doc if they haven't already been passed since the last used index
+            if (!one.containsKey(first)) {
+                one.put(first, index1);
+            }
+            if (!two.containsKey(second)) {
+                two.put(second, index2);
+            }
 
-        // Add the letters to the doc if they haven't already been passed since the last used index
-        if (!one.containsKey(first)) {
-            one.put(first, index1);
+            // Check if the current letter for doc2 has been passed in doc1
+            if (one.containsKey(second)) {
+                // If so, set the last used index of doc2 to the instance of the character
+                index1 = one.get(second);
+                // Then reset both of the HashMaps containing the characters
+                one.clear();
+                two.clear();
+                // Finally, increment the length of the substring by 1
+                length++;
+            }
+            // If not, check if the current letter for doc1 has been passed in doc2
+            if (two.containsKey(first)) {
+                // If so, set the last used index of doc2 to the instance of the character
+                index2 = two.get(first);
+                // Then reset both of the HashMaps containing the characters
+                one.clear();
+                two.clear();
+                // Finally, increment the length of the substring by 1
+                length++;
+            }
         }
-        if (!two.containsKey(second)) {
-            two.put(second, index2);
-        }
 
-        // Check if the current letter for doc2 has been passed in doc1
-        if (one.containsKey(second)) {
-            // If so, set the last used index of doc2 to the instance of the character
-            index1 = one.get(second);
-            // Then reset both of the HashMaps containing the characters
-            one.clear();
-            two.clear();
-            // Finally, increment the length of the substring by 1
-            length++;
-        }
-        // If not, check if the current letter for doc1 has been passed in doc2
-        if (two.containsKey(first)) {
-            // If so, set the last used index of doc2 to the instance of the character
-            index2 = two.get(first);
-            // Then reset both of the HashMaps containing the characters
-            one.clear();
-            two.clear();
-            // Finally, increment the length of the substring by 1
-            length++;
-        }
+        // Return the length of the longest substring
+        return length;
     }
-
-    // Return the length of the longest substring
-    return length;
 }
 
